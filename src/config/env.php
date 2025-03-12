fcd68504c381d0e56526cd9e4f6d585be168ac3a
MdIftiabMahmudNabil <mdiftiabmahmudnabil0@gmail.com>
2025-03-12 19:26:27 +0600

    Update env.php

--- src/config/env.php
+++ src/config/env.php
@@ -1,8 +1,7 @@
 <?php
-$host = 'dpg-cv8nfman91rc7381r17g-a.oregon-postgres.render.com';
-$db   = 'iubmdb';
-$user = 'iubuser';
+$host = 'dpg-cv8nfman91rc7381r17g-a.oregon-postgres.render.com'; 
+$db   = 'iubmdb';         
+$user = 'iubuser';        
 $pass = 'CvexCKIHd4zOXb3jDz8eNJU1jL5002DZ';
 
 $tmdb_api_key = '34addef368644c5b44f086432426b1d9';
-
