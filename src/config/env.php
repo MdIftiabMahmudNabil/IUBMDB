82268bfdf53356cd2cb4ab7939d7ad24657b6cb0
MdIftiabMahmudNabil <mdiftiabmahmudnabil0@gmail.com>
2025-03-12 19:19:44 +0600

    Update env.php

--- src/config/env.php
+++ src/config/env.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'db';
+$host = 'dpq-cv8nfman91rc7381r17g-a';
 $db   = 'iubmdb';
 $user = 'iubuser';
-$pass = 'iubpass';
+$pass = 'CvexCKIHd4zOXb3jDz8eNJU1jL5002DZ';
 
 $tmdb_api_key = '34addef368644c5b44f086432426b1d9';
