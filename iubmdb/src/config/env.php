725121e59d967442077d3bc8ba1da262e9812d77
MdIftiabMahmudNabil <mdiftiabmahmudnabil0@gmail.com>
2025-03-12 15:44:34 +0600

    Delete iubmdb directory

--- iubmdb/src/config/env.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = 'db';
-$db   = 'iubmdb';
-$user = 'iubuser';
-$pass = 'iubpass';
-
-$tmdb_api_key = '34addef368644c5b44f086432426b1d9';
--- iubmdb/src/views/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-setcookie('remember_user', '', time() - 3600, "/");
-header("Location: /views/login.php");
-exit;
