6f13c515da155f1973cff28556fb9b2d192bb881
MdIftiabMahmudNabil <mdiftiabmahmudnabil0@gmail.com>
2025-03-12 19:27:58 +0600

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -2,7 +2,8 @@
 require_once __DIR__ . '/env.php';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $dsn = "pgsql:host=$host;port=5432;dbname=$db";
+    $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("DB Error: " . $e->getMessage());
